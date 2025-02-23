0ff2612880e0f6840cf0b104312abd2162752134
lwhiteghostl <lwhiteghostl@gmail.com>
2025-02-23 20:03:41 +0500

    Ограничение роутов в зависимости от роли пользователя

--- client/src/components/routes/privateRoute.tsx
+++ client/src/components/routes/privateRoute.tsx
@@ -1,20 +1,26 @@
 import { Outlet, Navigate, useLocation } from 'react-router-dom';
 import { useAtomValue } from 'jotai';
-import { jwtState } from '@/atoms.ts';
+import { jwtState, userRoleState } from '@/atoms.ts';
 import React from 'react';
 
 type RouteProps = {
   children?: React.ReactNode;
+  accessRoles?: string[]
 };
 
-export const PrivateRoute: React.FC<React.PropsWithChildren<RouteProps>> = ({ children}) => {
+export const PrivateRoute: React.FC<React.PropsWithChildren<RouteProps>> = ({ children, accessRoles}) => {
   const auth = useAtomValue(jwtState);
+  const userRole = useAtomValue(userRoleState);
   const location = useLocation();
-  return auth? (
-    <>
-      {children}
-      <Outlet/>
-    </>
+  return (auth)? (
+      (!accessRoles || accessRoles?.includes(userRole!))? (
+        <>
+          {children}
+          <Outlet/>
+        </>
+      ) : (
+        <Navigate to={'/'} replace/>
+      )
   ) : (
     <Navigate to={'/login'} replace state={{from: location}}/>
   );
