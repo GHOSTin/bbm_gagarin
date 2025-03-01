585ad03e27700f734baec661073f12be000b2812
lwhiteghostl <lwhiteghostl@gmail.com>
2025-03-01 15:04:13 +0500

    Добавление api для управления сущностью профиля

---
+++ backend/src/profiles/dto/create-profile.dto.ts
@@ -0,0 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsDate, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class CreateProfileDto {
+  @ApiProperty()
+  @IsString()
+  firstName: string
+
+  @ApiProperty()
+  @IsString()
+  lastName: string
+
+  @ApiProperty()
+  @IsNumber()
+  userId: number
+
+  @ApiProperty({ required: false })
+  @IsString()
+  @IsOptional()
+  middleName?: string;
+
+  @ApiProperty({ required: false })
+  @IsNumber()
+  @IsOptional()
+  classNumber?: number;
+
+  @ApiProperty({ required: false })
+  @IsDate()
+  @IsOptional()
+  birthDate?: Date;
+
+  @ApiProperty()
+  @IsBoolean()
+  acceptTerms: false
+}
--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { PrismaModule } from '../prisma/prisma.module';
+import { PrismaModule } from '@/prisma/prisma.module';
 import { UsersController } from './users.controller';
+import { ProfilesModule } from '@/profiles/profiles.module';
+import { ProfilesService } from '@/profiles/profiles.service';
 
 @Module({
-  imports: [PrismaModule],
-  providers: [UsersService],
+  imports: [PrismaModule, ProfilesModule],
+  providers: [UsersService, ProfilesService],
   exports: [UsersService],
   controllers: [UsersController]
 })
